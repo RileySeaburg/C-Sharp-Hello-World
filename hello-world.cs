87352fdd802ebe7f056e526eb0ac876616216606
Riley Seaburg <riley@rileyseaburg.com>
2021-03-30 22:57:35 -0400

    first commit

---
+++ hello-world.cs
@@ -0,0 +1,7 @@
+// Class Header 
+public class Hello { 
+    // The program begins inside Main
+    public static void Main() {
+        System.Console.Out.WriteLine("Hello, World");
+    } // End of main
+} // End of Hello world
